f24512fd7e75e53af756646167329e8b62f8140a
lotanaharoni <lotanaharoni@gmail.com>
2021-06-24 16:10:09 +0300

    MainActivity

--- app/src/main/java/com/example/ex7_postpc/MainActivity.java
+++ app/src/main/java/com/example/ex7_postpc/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyLocalDb myLocalDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (myLocalDb == null){
+            myLocalDb = MyAppActivity.getLocalDb();
+        }
     }
 }
