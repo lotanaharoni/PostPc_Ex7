e2550f63e065814ebf8da0f1a20cdc6a6942d21d
lotanaharoni <lotanaharoni@gmail.com>
2021-06-24 23:24:02 +0300

    Fixed bug- myLocalDb not initialized

--- app/src/main/java/com/example/ex7_postpc/MainActivity.java
+++ app/src/main/java/com/example/ex7_postpc/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             myLocalDb = MyAppActivity.getLocalDb();
         }
         currentId = myLocalDb.getCurrentId();
+        int x = 3;
         if (currentId.equals("")){
             Intent newOrderIntent = new Intent(MainActivity.this, NewOrderActivity.class);
             startActivity(newOrderIntent);
