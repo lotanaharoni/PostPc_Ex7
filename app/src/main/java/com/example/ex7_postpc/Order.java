7dd8766cdfd7c79d11a1f6981970dd7093fb7b9a
lotanaharoni <lotanaharoni@gmail.com>
2021-06-24 16:17:42 +0300

    Added Order class

---
+++ app/src/main/java/com/example/ex7_postpc/Order.java
@@ -0,0 +1,37 @@
+package com.example.ex7_postpc;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Order implements Serializable {
+
+    public String id;
+    public String customerName;
+    public int pickles;
+    public boolean hummus;
+    public boolean tahini;
+    public String comment;
+    public String status;
+
+    public Order(){
+
+    }
+
+    public Order(String customerName, int pickles, boolean hummus, boolean tahini, String comment){
+        this.customerName = customerName;
+        this.pickles = pickles;
+        this.hummus = hummus;
+        this.tahini = tahini;
+        this.comment = comment;
+        this.id = UUID.randomUUID().toString();
+        this.status = "in progress";
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
