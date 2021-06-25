c9e270427bb2763f8a45a2c61fd6d0d14d5e81cf
lotanaharoni <lotanaharoni@gmail.com>
2021-06-25 22:49:03 +0300

    Added dependncies to the project

---
+++ app/src/test/java/com/example/ex7_postpc/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.example.ex7_postpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.mockito.Mockito;
+import org.robolectric.Robolectric;
+import static org.junit.Assert.assertEquals;
+
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 28)
+public class MainActivityTest {
+    @Test
+    public void first_test() {
+    }
+}
---
+++ app/src/test/java/com/example/ex7_postpc/NewOrderActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.ex7_postpc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 28)
+public class NewOrderActivityTest {
+    @Test
+    public void first_test() {
+    }
+}
